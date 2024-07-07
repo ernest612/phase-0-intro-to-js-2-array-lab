4fdc39c61ac66b53ef873181bfe948158363ccf0
ernestotieno95@gmail.com <ernest.otieno@student.moringaschool.com>
2024-07-08 01:39:27 +0300

    PUBLISH

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+    function destructivelyAppendCat(name) {
+        cats.push(name);
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift(name);
+    }
+    function destructivelyRemoveCat(name) {
+        cats.pop();
+    }
+    function destructivelyRemoveCat(){
+        cats.shift();
+    }
+    function appendCat(name) {
+        retun[cats, name];
+    }
+    function prependCat(name) {
+        return [name,...cats];
+    }
+    function removelastCat(){
+        return cats.slice(0, -1);
+    }
+    function removeFirstCat(){
+        return cats.slice(1);
+    }
+
+
